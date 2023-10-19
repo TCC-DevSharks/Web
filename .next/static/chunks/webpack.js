31da6f6fd0a00ce7f0d78282c5a94019b7908165
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-10-19 10:53:49 -0300

    cores médico

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70fefef2bac805da"; }
+/******/ 		__webpack_require__.h = function() { return "4da2c4ae02b8288e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
