e090360a34b15d5dc6f27a399632b64d5b7e1ef9
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-10-20 10:13:26 -0300

    painel de controle (sem o gráfico)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b97dfb27340cb75"; }
+/******/ 		__webpack_require__.h = function() { return "30ff00f92071fb08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
